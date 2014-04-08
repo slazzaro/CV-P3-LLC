e9a6201a60f9a1d8e9606f7eb594e61e512036af
saikatgomes <saikat@cs.wisc.com>
2014-04-08 14:27:56 -0500

    create main file

---
+++ code/main.m
@@ -0,0 +1,16 @@
+function [ ] = main( mainDir )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+    dirContents = dir(mainDir); % all dir contents
+    subFolders=[dirContents(:).isdir]; % just subfolder
+    folderNames = {dirContents(subFolders).name};    %subfolder names
+    folderNames(ismember(folderNames,{'.','..'})) = []; %remove . & ..
+    labels = containers.Map;    
+    for i=1:length(folderNames)
+        oneFolder=folderNames{i};
+        labels(oneFolder)=i;
+        %display(strcat(oneFolder,'-->',num2str(labels(oneFolder))));
+    end
+end
+
