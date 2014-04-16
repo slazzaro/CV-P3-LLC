d687b0f34f23971990f1e9d5cdd3fef27c7f28af
saikatgomes <saikat@cs.wisc.com>
2014-04-16 15:55:55 -0500

    super main function def

--- code/autoTest.m
+++ code/autoTest.m
@@ -2,8 +2,12 @@ function [  ] = autoTest( mainDir ,imgCount )
     for dict = [200 1024 2048]
         for pry = [2 3 4]
             for k = [5 9 13]
-                testNameNai=strcat('nai_',num2str(dict),'_',num2str(pry),'_',num2str(k));
-                testNameLLC=strcat('llc_',num2str(dict),'_',num2str(pry),'_',num2str(k));
+                testNameNai=strcat('N_',num2str(dict),'_',num2str(pry),'_',num2str(k));
+                testNameLLC=strcat('L_',num2str(dict),'_',num2str(pry),'_',num2str(k));
+                
+                MainSuper( mainDir ,imgCount, testName, useLLC, useKer, ...
+                           isSaved, dictionarySize, pyramidLevels, ...
+                           numTextonImages, patchSize, gridSpacing, k)
             end
         end
     end
