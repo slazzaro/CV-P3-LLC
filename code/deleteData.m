cf711e4cbc627a2342228f4a3735542370b67182
saikatgomes <saikat@cs.wisc.com>
2014-04-16 19:42:52 -0500

    minor edit to delOld

--- code/deleteData.m
+++ code/deleteData.m
@@ -1,5 +1,6 @@
 function [ ] = deleteData( parentDir )
-    dataDir=strcat(parentDir,'/data');
+    %dataDir=strcat(parentDir,'/data');
+    dataDir=parentDir;
     if exist(dataDir,'dir')
         display(strcat(datestr(now,'HH:MM:SS'),' [INFO] Removing :',dataDir));
         rmdir(dataDir,'s');      
