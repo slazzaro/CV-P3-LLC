b941cf70826a211b065d2d3a8bd41d8d90a6b6d1
saikatgomes <saikat@cs.wisc.com>
2014-04-17 17:23:38 -0500

    restructure folder

--- SpatialPyramid/BuildPyramid.m
+++ SpatialPyramid/BuildPyramid.m
@@ -44,8 +44,8 @@ if(~exist('params','var'))
     params.maxImageSize = 1000
     params.gridSpacing = 8
     params.patchSize = 16
-    %params.dictionarySize = 200
-    params.dictionarySize = 1024
+    params.dictionarySize = 200
+    %params.dictionarySize = 1024
     params.numTextonImages = 50
     params.pyramidLevels = 3
     params.oldSift = false;
